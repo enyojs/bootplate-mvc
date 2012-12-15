c7e7de3542b6e73a823a4f2fd3dda4a021838902
Jim Tang <jim.tang@palm.com>
2012-12-14 16:20:30 -0800

    Edited inline docs and rebuilt html with docco. Enyo-DCO-1.1-Signed-off-by: Jim Tang <jim.tang@palm.com>

--- source/controllers/EditorController.js
+++ source/controllers/EditorController.js
@@ -1,19 +1,20 @@
 
 // The `App.EditorController` kind
 // -------------------------------
-// The editor controller is responsible for setup of the content in
-// the `App.Editor` _view_. It extends `enyo.ModelController` and proxies
+// The editor controller is responsible for setting up the content in
+// the `App.Editor` view. It extends `enyo.ModelController` and proxies
 // the selected record.
 enyo.kind({
-    // There is a naming convention (not required) to keep presentation
-    // objects closely linked with their _controller_ layer counterparts by
-    // appending `Controller` to the end of the controller title.
+    // There is convention (not a requirement) to keep the names of
+    // presentation objects closely linked with those of their
+    // associated controllers. The controller name is typically formed
+    // by appending `"Controller"` to the name of the view.
     name: "App.EditorController",
-    // Make sure to check out the documentation for `enyo.ModelController`.
+    // Be sure to read the documentation for `enyo.ModelController`.
     kind: "enyo.ModelController",
-    // Even though the _textarea_ is in the _view_, we are owned by the _view_
-    // and all bubbled events will also be passed to us. We have access to the
-    // entirety of the view that owns us via the `owner` property.
+    // Even though the textarea is in the view, we are owned by the
+    // view and all bubbled events are also passed to us. We have access
+    // to the entirety of the view that owns us via the `owner` property.
     handlers: {
         oninput: "input",
         onready: "indexChanged"
@@ -22,29 +23,28 @@ enyo.kind({
        this.inherited(arguments);
        App.panels.addDispatchTarget(this);
     },
-    // So we map the selected index from the panels controller to
-    // our own `index` property.
+    // We map the selected index from the panels controller to our own
+    // `index` property.
     bindings: [
         {from: "App.panels.index", to: "index"}
     ],
-    // Note that we can setup a _changed_ event watcher for a property
-    // that only exists locally through bindings!
+    // Note that we can set up a `changed` event listener for a property
+    // that only exists locally through bindings.
     indexChanged: function () {
         var idx = this.index;
         var model = App.panels.at(idx);
         if (model && model !== this.model) this.set("model", model);
     },
     input: function () {
-        // Very, very important - let changes in the model propagate
-        // to changes in the view, obviously we interpreted a UI layer
-        // event for `oninput` but we translate the event into a model
-        // layer change and it will automatically cause the rest of the
-        // UI to update accordingly.
+        // Very important: Let changes in the model propagate to changes
+        // in the view. We translate a UI-layer `oninput` event into a
+        // model-layer change, and the rest of the UI is automatically
+        // updated to reflect the change.
         this.model.set({header: this.owner.$.input.get("value")});
     },
     // When the child view of the `owner` of this controller emits the
-    // _done_ named handler event it gets sent here to the controller
-    // since it exists.
+    // `done` named handler event, it gets sent here to the controller
+    // (since it exists).
     done: function () {
         // We are all done editing and can reset the state property.
         App.panels.set("isEditing", false);
--- source/controllers/PanelsController.js
+++ source/controllers/PanelsController.js
@@ -1,155 +1,144 @@
 
 // The `App.PanelsController` kind
 // -------------------------------
-// This is the kind definition of the global/singleton
-// application _panels controller_ `App.panels` that is instanced
-// in the `main` function in `Core.js`.
+// This is the kind definition for the global singleton application
+// panels controller (`App.panels`) instantiated in the `main` function
+// in `Core.js`.
 enyo.kind({
-    // We name it as a kind in the application namespace.
+    // We give the kind a name in the application namespace.
     name: "App.PanelsController",
-    // We subkind the `enyo.CollectionController` kind because
-    // we know we need the collection-controller built-in methods.
-    // Just note that even though it is a `enyo.CollectionController`
-    // we can add additional functionality to it because it is
-    // ultimately just another `enyo.Controller` with additional
-    // features.
+    // We base this kind on `enyo.CollectionController` because we know
+    // we need the collection controller's built-in methods. Even
+    // though this is an `enyo.CollectionController`, we can add
+    // functionality because it's ultimately just an `enyo.Controller`
+    // with additional features.
     kind: "enyo.CollectionController",
-    // In this application we already know what the collection kind
-    // is going to be for this controller, there is no global instance
-    // of the collection that is shared, we are going to hand this
-    // controller a kind reference and it will instantiate it when it
-    // is created
+    // We already know what the collection kind is going to be for this
+    // controller. Furthermore, we know that there will not be a shared
+    // global instance of the collection. We will hand this controller
+    // a kind reference and it will instantiate the collection kind when
+    // it is created.
     collection: "App.RollerCollection",
-    // While in `Enyo.js 2.2+` it is not required to create the
-    // `published` hash so that the corresponding getters/setters
-    // are created - it is still considered good practice if
-    // for no other reason than clarity and consistency. But it should
-    // be noted that calling `get` or `set` on any `enyo.Object` or
-    // subkind gives you the equivalent to _any property on the object_,
-    // can resolve paths, handle `enyo.Computed` properties and more.
-    // More on that another time, for now, we publish any publicly
-    // accessible properties.
+    // While in Enyo 2.2+ it is not required to create the `published`
+    // hash so that the corresponding getters and setters are created,
+    // this is still considered to be a best practice.
     published: {
-        // The entire application has state, one of these states
-        // is the editing state. While this property is designed to
-        // be used directly with the `App.Roller` _view_ kind, any
-        // other _component_ in the application can respond to it as
-        // we will see.
+        // The entire application has state. One possible state is the
+        // is the editing state. While the `isEditing` property is
+        // designed to be used directly with the `App.Roller` view kind,
+        // any other component in the application may respond to it as
+        // well (as we will see).
         isEditing: false,
-        // This is an example of abstracting common elements to
-        // the outermost-commonly-available-controller to be
-        // shared between objects in potentially different trees
-        // that have no direct connection but may share some state.
-        // In this case, some functionality in the application is
-        // separated because of how it needs to be represented in the
-        // DOM, but the elements both need to know what `index` the
-        // the _roller_ has selected. We present this property via
-        // `enyo.Bindings` from the outermost controller so any
-        // `enyo.View` or `enyo.Controller` can access it as needed.
-        // This value is actually bound to the `enyo.View` that implements
-        // the `enyo.Panels` that coordinate the presentation of these
+        // This is an example of abstracting common elements to the
+        // outermost commonly-available controller to be shared between
+        // objects that have no direct connection but may share some
+        // state. In this case, some of the application's functionality
+        // is separated because of how it needs to be represented in the
+        // DOM, but both elements need to know what `index` the roller
+        // has selected. Using bindings, we make this property available
+        // from the outermost controller, so that any instance of
+        // `enyo.View` or `enyo.Controller` may access it as needed.
+        //
+        // This value is actually bound to the view that implements the
+        // `enyo.Panels` that coordinates the presentation of these
         // models in the `enyo.Collection`. The binding to this property
-        // is actually initiated in the view, though.
+        // is initiated in the view.
         index: 0,
-        // Simple boolean to keep track of whether or not the animation
-        // is running (_is started_).
+        // A simple boolean to keep track of whether or not the animation
+        // is running
         isStarted: false
     },
-    // The timer reference as returned by `setInterval`.
+    // The timer reference as returned by `setInterval`
     timer: null,
-    // We listen for one special event to trigger that we're ready
+    // We listen for the special `ready` event to signal that we're ready
     // to start inspecting and listening on our property observers.
     handlers: {
         onready: "collectionChanged"
     },
-    // Here we have some methods that are actually _dispatch event targets_
-    // of an `enyo.View` (the toolbar). We'll see how that works when we
-    // examine that view, but suffice it to say these methods are executed
-    // because of _bubbled events_ from a part of the view layer.
+    // The following methods are actually dispatch event targets of an
+    // `enyo.View` (the toolbar). These methods are executed in response
+    // to events bubbled from the view layer.
     //
-    // When the `edit` button is tapped, we change our state to editing.
+    // When the `"Edit"` button is tapped, we change to the editing state.
     editModel: function () {
-        // We use the `set` setter even though we published the property.
+        // We use the `set` method even though we've published the property.
         this.set("isEditing", true);
     },
-    // When the `add` button is tapped, we add a new model to the collection
-    // with the default properties as defined in the `enyo.Model` kind.
+    // When the `"Add"` button is tapped, we add a new model to the
+    // collection.  Its properties have the default values defined in
+    // `enyo.Model`.
     addModel: function () {
         // Here we call the `add` method that is proxying the underlying
-        // collection API. By passing the empy hash, it knows to create the
-        // model with all of the defaults.
+        // collection API. Because we pass in an empty hash, it knows to
+        // create the model using all of the defaults.
         this.add({});
     },
-    // When the `next` button is tapped we have some additional things to
-    // do to prepare the operation.
+    // When the `"Next"` button is tapped, we have some additional things
+    // to do to prepare the operation.
     nextModel: function () {
-      // We want to freeze the current timer-driven operation/animation.
+      // We freeze the current timer-driven operation/animation.
       this.stop();
-      // We call our `next` method to select the next index appropriately.
+      // We call our `next` method to select the next index.
       this.next();
-      // Now we restart the timer operation to ensure the animation will
+      // Then we restart the timer operation to ensure the animation will
       // continue.
       if (!this.isEditing) this.start();
     },
-    // Begin animation of any of the panels for the roller.
+    // Begins animation of any of the panels for the roller.
     start: function () {
-        // If we're already started, we don't have anything to do.
+        // If we're already started, we have nothing to do.
         if (this.isStarted) return;
-        // Assign the `timer` property as we create our loop to
-        // animate every 5 seconds. Great opportunity to use `enyo.bind`.
+        // Assign the `timer` property as we create a loop to animate
+        // every 5 seconds. A great opportunity to use `enyo.bind`.
         this.timer = setInterval(enyo.bind(this, this.next), 5000);
-        // Just in case anyone is listening we'll use the setter
-        // to trigger them. It is good practice to do this so that
-        // if you (or anyone else) adds a listener later you aren't
-        // scratching your head wondering why the value is changing
-        // but the `enyo.Observer` is being notified! There are
-        // cases where it is desirable _not_ to use the setter.
+        // We'll use the setter to notify anyone who happens to be
+        // listening. It is good practice to do this so that if you add
+        // a listener later you won't wonder why the value is changing
+        // but the observer is being notified. (Though there are cases
+        // in which it's desirable _not_ to use the setter.)
         this.set("isStarted", true);
     },
-    // Stop animation if we are animating.
+    // Stops animation if we are animating.
     stop: function () {
         // Clear the interval timer.
         if (this.timer) clearInterval(this.timer);
-        // Reset our `isStarted` property, again
-        // using the setter so we trigger listeners if
-        // there are any.
+        // Reset our `isStarted` property, again using the setter
+        // so that we trigger listeners (if there are any).
         this.set("isStarted", false);
     },
-    // Select our next index.
+    // Selects our next index.
     next: function () {
         // Notice we're using and setting our local `index`
-        // property that is part of a _two-way_ `enyo.Binding`.
+        // property, which is part of a two-way binding.
         var idx = this.get("index");
         var len = this.get("length");
-        // If the next value for index goes beyond the number
-        // of available panels, reset it to _0_
-        // so we can start over.
+        // If the next value for index goes beyond the number of
+        // available panels, reset it to 0 so we can start over.
         if (idx+1 === len) {
           this.set("index", 0);
         } else {
-          // Otherwise let's increment the index and move forward.
+          // Otherwise, increment the index and move forward.
           this.set("index", ++idx);
         }
     },
-    // Listen for and allow us to respond to _editing state_ changes
-    // using the default behavior that if the `isEditing` property
-    // is modified any `isEditingChanged` method will be called.
+    // Listens for and lets us respond to changes in editing state.
+    // Relies on the default behavior that if the `isEditing` property
+    // is modified, the `isEditingChanged` method will be called (if
+    // such a method exists).
     isEditingChanged: function () {
-        // If we are indeed editing now we want to stop animation
-        // pronto!
+        // If we are editing now, stop animating.
         if (this.isEditing === true) this.stop();
-        // Otherwise we should start animating because we just left
-        // the editing state.
+        // Otherwise, start animating because we just left the editing
+        // state.
         else this.start();
     },
-    // Here we listen and respond to the _length change_ notification
-    // so we can automatically begin animating when appropriate.
+    // Listens for length change notifications, so we can automatically
+    // begin animating when appropriate.
     lengthChanged: function () {
-        // If the length is greater than a single panel let's
-        // go ahead and start 'er up.
+        // If the length (i.e., the number of panels) is greater than
+        // 1, start animating.
         if (this.length > 1) this.start();
-        // If the length has fallen to 1 (or lower?) let's
-        // stop animating.
+        // If the length has fallen to 1, stop animating.
         if (this.length <= 1) this.stop();
     }
 });
