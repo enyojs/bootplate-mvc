2dfb13386368a5c729f888b8078fb755bb242e74
Olaf Conradi <olaf@conradi.org>
2013-01-06 03:37:43 +0100

    Delay model loading for debug.html to work

--- source/models/models/RollerModel.js
+++ source/models/models/RollerModel.js
@@ -3,9 +3,11 @@
 // --------------------------
 // Models are native Backbone models and should be implemented
 // according to their API.
-Sample.RollerModel = Backbone.Model.extend({
-    defaults: {
-        header: "You just created a new record!",
-        editing: false
-    }
+enyo.ready(function() {
+    Sample.RollerModel = Backbone.Model.extend({
+        defaults: {
+            header: "You just created a new record!",
+            editing: false
+        }
+    });
 });
