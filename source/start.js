ebee77014b3726944fa3c4e9704f5f55f44b34cb
Jim Tang <jim.tang@palm.com>
2013-01-08 15:54:10 -0800

    Additional refinement of comments. Enyo-DCO-1.1-Signed-off-by: Jim Tang <jim.tang@palm.com>

--- source/start.js
+++ source/start.js
@@ -8,22 +8,22 @@
 // executed once the `document` is completely ready.
 enyo.ready(function () {
     // This is a very imporant declaration, in which we instantiate our
-    // application. We arbitrarily assign it to a global variable called
-    // `app` for reference, in case we need it later. All other
-    // components of this application are instantiated in the `Sample`
-    // namespace. See the `apps/Sample.js` file for more information on
-    // namespaces and what this actually means.
+    // application. We arbitrarily assign the instance to a global
+    // variable called `app`, in case we need to refer to it later. All
+    // other components of this application are instantiated in the
+    // `Sample` namespace. See the `apps/Sample.js` file for more
+    // information on namespaces and what this actually means.
     app = new Sample.Application();
     // When our application renders for the first time, we want it to
     // have some sample data to display. We'll use data that we've
-    // prepared specifically for this purpose and stored in the file
+    // prepared specifically for this purpose in the file
     // `models/Scaffold.js`.
     Sample.panels.add(Sample.Scaffold.Roller);
-    // Now that we have data for the view layer to use, let's go ahead
-    // and render it to the DOM. We could have chosen to have the
-    // application automatically render upon creation, but in
-    // `apps/Sample.js` we deliberately told it not to do so. This
-    // let us to prepare our scaffold data before the view layer was
+    // Now that we have data for the view layer to display, let's go
+    // ahead and render it to the DOM. We could have had the application
+    // automatically render upon creation, but in `apps/Sample.js` we
+    // deliberately told it not to do so. This gave us the opportunity
+    // to prepare our scaffold data before the view layer was
     // initialized.
     app.render();
 });
