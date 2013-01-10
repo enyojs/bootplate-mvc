25ce64f8c96699774703b681c93f0392c78284ce
Cole Davis <cole.davis@palm.com>
2013-01-09 16:41:51 -0800

    update documentation and regenerate docs and json manifest
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@palm.com>

--- source/models/collections/NodeCollection.js
+++ source/models/collections/NodeCollection.js
@@ -1,4 +1,10 @@
 
+// The `Sample.NodeCollection`
+// ---------------------------
+// The _Sample.NodeCollection_ uses the built-in _sync_ mechanism
+// of _Backbone_ by setting a _url_ value. In this case it is pointing
+// directly at a JSON manifest of data from which it will constrcuct the
+// records for the file-tree.
 enyo.ready(function () {
     Sample.NodeCollection = Backbone.Collection.extend({
         model: Sample.NodeModel,
