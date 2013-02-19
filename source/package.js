2c082aa38065516052d3745a3dcd2b5ee0a5efe7
Cole Davis <cole.davis@palm.com>
2013-02-19 11:34:26 -0800

    add backbone-support library
    Enyo-DCO-1.1-Signed-off-by: Cole Davis <cole.davis@palm.com>

--- source/package.js
+++ source/package.js
@@ -2,7 +2,7 @@ enyo.depends(
 	"$lib/layout",
 	"$lib/onyx",    // To theme Onyx using Theme.less, change this line to $lib/onyx/source,
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
-    "$lib/mvc",
+    "$lib/backbone-support",
 	"app.css",
 	"app.js",
     "start.js"
